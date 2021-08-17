ad644ee843c809bbb7eb622324cc624507fb2108
Ethan Ngo <38741763+Domaki@users.noreply.github.com>
2021-08-16 22:01:20 -0700

    Minor changes beforee uploading
    Fixed fixed objects that shouldve been ground, look sensitivity reduced

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene("MenuLoad");
+        SceneManager.LoadScene("Sandbox Level");
     }
 
     public void Level()
